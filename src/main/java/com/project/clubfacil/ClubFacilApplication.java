0c5e0cd224ba176d175d91954f949a20a11814ae
ocampo320 <telecentro.lasierra@gmail.com>
2021-12-12 17:28:07 -0500

    se crea proyecto inicial pasarela de pagos

---
+++ src/main/java/com/project/clubfacil/ClubFacilApplication.java
@@ -0,0 +1,15 @@
+package com.project.clubfacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubFacilApplication {
+
+    public static void main(String[] args) {
+        System.out.printf("Se inicio la app");;
+        SpringApplication.run(ClubFacilApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/project/clubfacil/ClubFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.clubfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubFacilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
