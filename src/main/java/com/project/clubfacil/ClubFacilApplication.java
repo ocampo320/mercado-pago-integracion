ed97fdc2f872afe764094c8987da89ee9c006c8e
ocampo320 <telecentro.lasierra@gmail.com>
2021-12-14 22:18:25 -0500

    Se inicia flujo de consulta tipos de documentos

--- src/main/java/com/project/clubfacil/ClubFacilApplication.java
+++ src/main/java/com/project/clubfacil/ClubFacilApplication.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.clubfacil.model.IdentificationTypes;
 import com.project.clubfacil.repository.IdentificationTypesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,8 +18,8 @@ import java.util.Arrays;
 @SpringBootApplication
 public class ClubFacilApplication {
 
-    @Autowired
-    IdentificationTypesRepository identificationTypesRepository;
+
+
 
     public static void main(String[] args) {
         System.out.printf("Se inicio la app");
@@ -31,33 +32,32 @@ public class ClubFacilApplication {
         return builder.build();
     }
 
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return args -> {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization","Bearer APP_USR-3951552830330174-121221-5adc5eb48689499ac13c310710d71299-109891437");
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            HttpEntity<IdentificationTypes> entity = new HttpEntity<IdentificationTypes>(headers);
-
-            ResponseEntity<IdentificationTypes[]> response3 = restTemplate.exchange("https://api.mercadopago.com/v1/identification_types", HttpMethod.GET, entity, IdentificationTypes[].class);
-            if(response3.getStatusCode().equals(HttpStatus.OK))
-            {
-                Arrays.stream(response3.getBody()).forEach(identificationTypes -> {
-                    IdentificationTypes identificationTypesNew=new IdentificationTypes();
-                    identificationTypesNew.setType(identificationTypes.getType());
-                    identificationTypesNew.setMax_length(identificationTypes.getMax_length());
-                    identificationTypesNew.setMin_length(identificationTypes.getMin_length());
-                    identificationTypesNew.setName(identificationTypes.getName());
-                    identificationTypesNew.setId(identificationTypes.getId());
-                    identificationTypesRepository.save(identificationTypesNew);
-                    System.out.println(identificationTypes.getName());
-                });
-
-            }
-
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        return args -> {
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.set("Authorization","Bearer APP_USR-3951552830330174-121221-5adc5eb48689499ac13c310710d71299-109891437");
+//            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//            HttpEntity<IdentificationTypes> entity = new HttpEntity<IdentificationTypes>(headers);
+//
+//            ResponseEntity<IdentificationTypes[]> response3 = restTemplate.exchange("https://api.mercadopago.com/v1/identification_types", HttpMethod.GET, entity, IdentificationTypes[].class);
+//            if(response3.getStatusCode().equals(HttpStatus.OK))
+//            {
+//                Arrays.stream(response3.getBody()).forEach(identificationTypes -> {
+//                    IdentificationTypes identificationTypesNew=new IdentificationTypes();
+//                    identificationTypesNew.setType(identificationTypes.getType());
+//                    identificationTypesNew.setMax_length(identificationTypes.getMax_length());
+//                    identificationTypesNew.setMin_length(identificationTypes.getMin_length());
+//                    identificationTypesNew.setName(identificationTypes.getName());
+//                    identificationTypesNew.setId(identificationTypes.getId());
+//                    identificationTypesRepository.save(identificationTypesNew);
+//                    System.out.println(identificationTypes.getName());
+//                });
+//            }
+//
+//
+//        };
+//    }
 
 }
--- src/main/java/com/project/clubfacil/security/MainSecurity.java
+++ src/main/java/com/project/clubfacil/security/MainSecurity.java
@@ -1,75 +1,63 @@
-package com.project.clubfacil.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MainSecurity extends WebSecurityConfigurerAdapter {
-
+//package com.project.clubfacil.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class MainSecurity extends WebSecurityConfigurerAdapter {
+//
+////    @Autowired
+////    UserDetailsServiceImpl userDetailsService;
+//
 //    @Autowired
-//    UserDetailsServiceImpl userDetailsService;
-
-    @Autowired
-    JwtEntryPoint jwtEntryPoint;
-
-    @Bean
-    public JwtTokenFilter jwtTokenFilter() {
-        return new JwtTokenFilter();
-    }
-
+//    JwtEntryPoint jwtEntryPoint;
+//
 //    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-
-
-
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
+//    public JwtTokenFilter jwtTokenFilter() {
+//        return new JwtTokenFilter();
 //    }
 //
+////    @Bean
+////    public PasswordEncoder passwordEncoder() {
+////        return new BCryptPasswordEncoder();
+////    }
+//
+//
+//
+////    @Bean
+////    @Override
+////    public AuthenticationManager authenticationManagerBean() throws Exception {
+////        return super.authenticationManagerBean();
+////    }
+////
+////    @Override
+////    protected AuthenticationManager authenticationManager() throws Exception {
+////        return super.authenticationManager();
+////    }
+//
 //    @Override
-//    protected AuthenticationManager authenticationManager() throws Exception {
-//        return super.authenticationManager();
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/identificationTypes/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+//                .and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 //    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
-                .antMatchers("/building/**").permitAll()
-                .antMatchers("/manager/**").permitAll()
-                .antMatchers("/floor/**").permitAll()
-                .antMatchers("/apartment/**").permitAll()
-                .antMatchers("/photo/**").permitAll()
-                .antMatchers("/gender/**").permitAll()
-                .antMatchers("/visitor/**").permitAll()
-                .antMatchers("/profession/**").permitAll()
-                .antMatchers("/rol/**").permitAll()
-                .antMatchers("/document/**").permitAll()
-                .antMatchers("/employee/**").permitAll()
-                .antMatchers("/collaborator/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-}
+//}
