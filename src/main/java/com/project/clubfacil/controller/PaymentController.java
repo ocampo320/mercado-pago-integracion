3ee884b8a12d3e692f34710e0cce0027b6f769aa
ocampo320 <telecentro.lasierra@gmail.com>
2021-12-24 15:27:32 -0500

    seajusta de respuesta de consulta depago

--- src/main/java/com/project/clubfacil/controller/PaymentController.java
+++ src/main/java/com/project/clubfacil/controller/PaymentController.java
@@ -1,12 +1,13 @@
 package com.project.clubfacil.controller;
 
-import com.project.clubfacil.model.IdentificationTypes;
-import com.project.clubfacil.services.IdentificationTypesServices;
+import com.project.clubfacil.dtos.paymentsResponseDto.PaymentResponse;
 import com.project.clubfacil.services.PaymentServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/payments")
@@ -17,12 +18,14 @@ public class PaymentController {
     PaymentServices paymentServices;
 
     @GetMapping("/{paymentId}")
-    public void getIdentificationTypes(@PathVariable("paymentId") String paymentId) {
+    public ResponseEntity<PaymentResponse> getIdentificationTypes(@PathVariable("paymentId") String paymentId, HttpServletRequest request) {
+        PaymentResponse paymentResponse = new PaymentResponse();
 //        String token = request.getHeader(tokenHeader);
 //        String username = jwtProvider.getNombreUsuarioFromToken(token);
 //        String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
 //        List<ApartmentResponse> list = apartmentServices.findApartmentByFloor(id);
-         paymentServices.getPayment( paymentId);
+        paymentResponse = paymentServices.getPayment(paymentId);
+        return new ResponseEntity(paymentResponse, HttpStatus.OK);
     }
 }
 
--- src/main/java/com/project/clubfacil/services/PaymentServices.java
+++ src/main/java/com/project/clubfacil/services/PaymentServices.java
@@ -1,10 +1,7 @@
 package com.project.clubfacil.services;
 
+import com.project.clubfacil.dtos.paymentsResponseDto.PaymentResponse;
 import com.project.clubfacil.dtos.paymentsResponseDto.PaymentResponseDto;
-import com.project.clubfacil.dtos.preferencesRequestDTO.PreferencesResponseDto;
-import com.project.clubfacil.dtos.preferencesRequestDTO.RootRequestDto;
-import com.project.clubfacil.dtos.preferencesResponseDto.RootResponseDto;
-import com.project.clubfacil.model.IdentificationTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -23,10 +20,9 @@ public class PaymentServices {
     RestTemplate restTemplate;
 
 
-    public void getPayment(String code) {
+    public PaymentResponse  getPayment(String code) {
         UriComponentsBuilder   builder = UriComponentsBuilder.fromHttpUrl("https://api.mercadopago.com/v1/payments/")
                 .path(code);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Authorization", "Bearer TEST-4092050722626379-122214-5e7417392c6cb2a54c93f26ecf22d9f2-109891437");
@@ -39,10 +35,18 @@ public class PaymentServices {
         ResponseEntity<PaymentResponseDto> response = restTemplate.exchange(uriComponents.toString(),  HttpMethod.GET, request, PaymentResponseDto.class);
 
 
-        System.out.println(response.getBody().transaction_details.installment_amount);
-        System.out.println(response.getBody());
+        PaymentResponse paymentResponse=new PaymentResponse();
+
+
+        paymentResponse.setStatus(response.getBody().status);
+        paymentResponse.setDescriptionProduct(response.getBody().description);
+        paymentResponse.setTransactionAmount(response.getBody().transaction_amount);
+
+return  paymentResponse;
 
     }
+
+
 
 
 }
